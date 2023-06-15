966b875ab6017a0ee42b422a176dbe91ac245c2e
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-15 07:53:50 -0700

    Update 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (1);
 }
-
