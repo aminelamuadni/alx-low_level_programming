0ce16f47894f0118d08a38ad0a20609725e6f52b
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 04:01:50 -0700

    Add 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; ++i)
+		_putchar(text[i]);
+	_putchar('\n');
+
+	return (0);
+}
+
