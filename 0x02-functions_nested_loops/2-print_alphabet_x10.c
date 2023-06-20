212a1cf52c9b7872273cf6ce02eb1c8950089e5e
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 04:15:13 -0700

    Add 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	int count, letter;
+
+	for (count = 0; count <= 9; ++count)
+	{
+		for (letter = 'a'; letter <= 'z'; ++letter)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
+
