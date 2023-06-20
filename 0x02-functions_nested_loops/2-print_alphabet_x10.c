7349595935054b89d2f145acfa6f38aa77797562
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 04:21:04 -0700

    Update 2-print_alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int count;
 	int letter;
 
-	for (count = 0; count <= 9; ++count)
+	for (count = 0; count < 10; ++count)
 	{
 		for (letter = 'a'; letter <= 'z'; ++letter)
 			_putchar(letter);
