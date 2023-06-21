a013d3f1637e29f32c709ea4f80c34a03ead1078
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 18:20:50 -0700

    Update 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,20 @@
 int main(void)
 {
 	int count;
-	unsigned int a = 1, b = 2, next;
+	unsigned long long num1 = 1, num2 = 2, next;
 
-	printf("%d, %d, ", a, b);
+	printf("%llu, %llu, ", num1, num2);
 
 	for (count = 2; count < 98; count++)
 	{
-		next = a + b;
-		printf("%u", next);
+		next = num1 + num2;
+		printf("%llu", next);
 
 		if (count != 97)
 			printf(", ");
 
-		a = b;
-		b = next;
+		num1 = num2;
+		num2 = next;
 	}
 
 	printf("\n");
