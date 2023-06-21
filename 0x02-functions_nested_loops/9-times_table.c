0a14b28881f6e5b92bcd23b4f8cbb9660bdc5453
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 17:23:47 -0700

    Add 9-times_table.c file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; ++row)
+	{
+		_putchar(48);
+		for (column = 1; column <= 9; ++column)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = row * column;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + 48);
+
+			_putchar((product % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
+
