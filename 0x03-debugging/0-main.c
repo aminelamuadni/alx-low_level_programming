506d6120d332dd45ed3ddc8f0dfdcb50b4893e7b
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 18:35:46 -0700

    Add 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
+
