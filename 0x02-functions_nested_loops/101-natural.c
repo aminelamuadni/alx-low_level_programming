9d50378b5a158736dd04348d0a68998338f57b4b
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-20 17:44:19 -0700

    Add 101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
