6eb940cc51ba0d7b373a2648fcac11b17e84313b
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-22 18:44:55 -0700

    Update 10-print_triangle.c file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,19 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		for (j = 0; j <= i; j++)
-			_putchar('#');
-
-		_putchar('\n');
+		for (i = 1; i <= size; ++i)
+		{
+			for (j = 1; j <= size; ++j)
+			{
+				if ((i + j) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
