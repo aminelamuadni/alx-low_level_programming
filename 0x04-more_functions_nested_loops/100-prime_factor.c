4ac73821163e80fa814003d5c58b9b5af6590357
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-22 18:53:00 -0700

    Update 100-prime_factor.c file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,33 +9,37 @@
 
 unsigned long largest_prime_factor(unsigned long num)
 {
-    unsigned long factor = 2;
-    unsigned long largest_factor = 2;
+	unsigned long factor = 2;
+	unsigned long largest_factor = 2;
 
-    while (num > 1)
-    {
-        if (num % factor == 0)
-        {
-            largest_factor = factor;
-            num /= factor;
-            while (num % factor == 0)
-            {
-                num /= factor;
-            }
-        }
-        factor++;
-    }
+	while (num > 1)
+	{
+		if (num % factor == 0)
+		{
+			largest_factor = factor;
+			num /= factor;
+			while (num % factor == 0)
+				num /= factor;
+		}
+		factor++;
+	}
 
-    return largest_factor;
+	return (largest_factor);
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-    unsigned long number = 612852475143;
-    unsigned long largest_factor = largest_prime_factor(number);
+	unsigned long number = 612852475143;
+	unsigned long largest_factor = largest_prime_factor(number);
 
-    printf("%lu\n", largest_factor);
+	printf("%lu\n", largest_factor);
 
-    return 0;
+	return (0);
 }
 
