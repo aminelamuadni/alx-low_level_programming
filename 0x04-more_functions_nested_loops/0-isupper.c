1c939fa9ff18875b0d455ba005db37a85df7e3df
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-22 18:03:43 -0700

    Update 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,10 @@
  *
  * Return: 1 if the character is uppercase, 0 otherwise.
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
