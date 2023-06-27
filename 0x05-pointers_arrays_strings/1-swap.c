b3af3f3e61c055dd08e692fc5554de05a13210fe
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-27 06:06:28 -0700

    Add 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to an integer
+ * @b: pointer to another integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
