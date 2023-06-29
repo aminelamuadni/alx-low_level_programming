a631d04f5f39848c0294ea18a960863c0a1eeb64
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 12:44:42 -0700

    Update 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,15 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 
 	/* Find the end of the destination string */
-	while (dest[i] != '\0')
+	while (dest[i])
 		i++;
 
 	/* Append the source string to the destination string */
 	for (j = 0; src[j] != '\0'; j++)
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 
 	/* Null-terminate the destination string */
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
