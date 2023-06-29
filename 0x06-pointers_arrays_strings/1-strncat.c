f3bb50d77e1e1bef5d68915b9858f305a4a641f3
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 12:47:33 -0700

    Add 1-strncat.c file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The number of bytes to use from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	/* Find the end of the destination string */
+	while (dest[i])
+		i++;
+
+	/* Append the first n characters of the source string to the destination string */
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	/* Null-terminate the destination string */
+	dest[i + j] = '\0';
+
+	return (dest);
+}
