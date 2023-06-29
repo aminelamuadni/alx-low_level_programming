b606f7dfca03519d666c46a2201e0fc2f5770ce3
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 12:52:38 -0700

    Update 1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = 0;
-
 	/* Find the end of the destination string */
-	while (dest[i])
-		i++;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
 	/* Append the first n characters of the source string to the destination string */
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[i + j] = src[j];
 
-	/* Null-terminate the destination string */
-	dest[i + j] = '\0';
+	/* If src has less than n characters, null-terminate dest */
+	if (j < n)
+		dest[i + j] = '\0';
 
 	return (dest);
 }
