ecbe1bfd1d240cce818d824ed736b2d884a558dc
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 15:13:52 -0700

    Update 103-infinite_add.c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,38 +15,51 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + len1) != '\0')
 		len1++;
+
 	while (*(n2 + len2) != '\0')
 		len2++;
+
 	if (len1 >= len2)
 		bigger = len1;
 	else
 		bigger = len2;
+
 	if (size_r <= bigger + 1)
 		return (0);
+
 	r[bigger + 1] = '\0';
+
 	len1--, len2--, size_r--;
+
 	i = *(n1 + len1) - 48, j = *(n2 + len2) - 48;
+
 	while (bigger >= 0)
 	{
 		op = i + j + carry;
+
 		if (op >= 10)
 			carry = op / 10;
 		else
 			carry = 0;
+
 		if (op > 0)
-		*(r + bigger) = (op % 10) + 48;
+			*(r + bigger) = (op % 10) + 48;
 		else
 			*(r + bigger) = '0';
+
 		if (len1 > 0)
 			len1--, i = *(n1 + len1) - 48;
 		else
 			i = 0;
+
 		if (len2 > 0)
 			len2--, j = *(n2 + len2) - 48;
 		else
 			j = 0;
+
 		bigger--, size_r--;
 	}
+
 	if (*(r) == '0')
 		return (r + 1);
 	else
