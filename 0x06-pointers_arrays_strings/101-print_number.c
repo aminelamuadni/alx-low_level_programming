199bc1a06fd8efcd564dff3bddfc68e59c1ab6af
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 13:40:01 -0700

    Add 101-print_number.c file

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+/**
+ * print_number - print an integer.
+ * @n: integer to be printed.
+ *
+ * Return: Nothing.
+ */
+void print_number(int n)
+{
+    unsigned int m, d, count;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        m = n * -1;
+    }
+    else
+    {
+        m = n;
+    }
+
+    d = m;
+    count = 1;
+
+    while (d > 9)
+    {
+        d /= 10;
+        count *= 10;
+    }
+
+    while (count >= 1)
+    {
+        _putchar(((m / count) % 10) + '0');
+        count /= 10;
+    }
+}
