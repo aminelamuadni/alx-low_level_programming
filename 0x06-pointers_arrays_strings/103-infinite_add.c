c42347494dc54b56e7422dc449023e7f784ddab6
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 14:08:58 -0700

    Update 103-infinite_add.c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,47 +11,54 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int len1 = 0, len2 = 0, sum, max_length, carry = 0;
+	int digit1, digit2;
 
-	for (i = 0; n1[i] != '\0'; i++)
-		;
-	for (j = 0; n2[j] != '\0'; j++)
-		;
-	if (i > j)
-		l = i;
+	while (*(n1 + len1) != '\0')
+		len1++;
+	while (*(n2 + len2) != '\0')
+		len2++;
+
+	if (len1 >= len2)
+		max_length = len1;
 	else
-		l = j;
-	if (size_r <= l + 1)
+		max_length = len2;
+
+	if (size_r <= max_length + 1)
 		return (0);
-	m = 0;
-	for (k = 0; k <= l; k++)
+
+	r[max_length + 1] = '\0';
+	len1--, len2--, size_r--;
+	digit1 = *(n1 + len1) - 48, digit2 = *(n2 + len2) - 48;
+
+	while (max_length >= 0)
 	{
-		n = m;
-		if (i > 0)
-			n += n1[--i] - '0';
-		if (j > 0)
-			n += n2[--j] - '0';
-		if (n > 9)
-		{
-			m = 1;
-			r[k] = (n % 10) + '0';
-		}
+		sum = digit1 + digit2 + carry;
+		if (sum >= 10)
+			carry = sum / 10;
 		else
-		{
-			m = 0;
-			r[k] = n + '0';
-		}
+			carry = 0;
+
+		if (sum > 0)
+			*(r + max_length) = (sum % 10) + 48;
+		else
+			*(r + max_length) = '0';
+
+		if (len1 > 0)
+			len1--, digit1 = *(n1 + len1) - 48;
+		else
+			digit1 = 0;
+
+		if (len2 > 0)
+			len2--, digit2 = *(n2 + len2) - 48;
+		else
+			digit2 = 0;
+
+		max_length--, size_r--;
 	}
-	if (m == 1)
-		r[l] = '1';
+
+	if (*r == '0')
+		return (r + 1);
 	else
-		l--;
-	for (k = 0; k <= l / 2; k++)
-	{
-		m = r[k];
-		r[k] = r[l - k];
-		r[l - k] = m;
-	}
-	r[l + 1] = '\0';
-	return (r);
+		return r;
 }
