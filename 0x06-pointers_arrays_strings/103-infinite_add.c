34c4fa3c0cb5972df16d7e2dc4ced484646fac4d
Amine Lamuadni <W7equwE5@gmail.com>
2023-06-29 13:56:18 -0700

    Add 103-infinite_add.c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,46 +2,45 @@
 
 /**
  * infinite_add - adds two numbers
- * @n1: first input string
- * @n2: second input string
+ * @n1: string number
+ * @n2: string number
  * @r: buffer that the function will use to store the result
  * @size_r: buffer size
- *
- * Return: pointer to the result.
+ * Return: pointer to result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
-	int carry = 0;
-
-	for (i = 0; n1[i] != '\0'; i++)
-		;
-	for (j = 0; n2[j] != '\0'; j++)
-		;
-	if (i > size_r || j > size_r)
+	int len1, len2, bigger, op, ad = 0;
+	
+	len1 = 0;
+	while (n1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (n2[len2] != '\0')
+		len2++;
+	bigger = (len1 >= len2) ? len1 : len2;
+	if (size_r <= (bigger + 1))
 		return (0);
-	i--;
-	j--;
-	l = 0;
-	for (k = 0; k < size_r - 1; k++, i--, j--, l++)
+	r[bigger + 1] = '\0';
+	len1--;
+	len2--;
+	while (bigger >= 0)
 	{
-		m = (i >= 0) ? n1[i] - '0' : 0;
-		n = (j >= 0) ? n2[j] - '0' : 0;
-		if ((m + n + carry) / 10 != 0)
-			r[l] = (m + n + carry) % 10 + '0', carry = 1;
+		op = ad;
+		if (len1 >= 0)
+			op += n1[len1--] - '0';
+		if (len2 >= 0)
+			op += n2[len2--] - '0';
+		if (op >= 10)
+		{
+			ad = 1;
+			op -= 10;
+		}
 		else
-			r[l] = (m + n + carry) + '0', carry = 0;
+			ad = 0;
+		r[bigger--] = (op + '0');
 	}
-	if (carry == 1)
-		r[l] = 1 + '0', l++;
-	if (l == size_r)
-		return (0);
-	r[l] = '\0';
-	for (i = 0, j = l - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
+	if (r[0] == '0')
+		return (r + 1);
 	return (r);
 }
