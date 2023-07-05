498a6957badf1b89abdf668d7906acb40d61aa96
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-05 14:52:15 -0700

    Update 101-wildcmp.c file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,22 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp - function that compares two strings
- * @s1: string to compare
- * @s2: string to compare with possible wildcard(s)
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * Return: 1 if considered identical, otherwise 0
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0') /* if end of both strings, they match */
-        return (1);
-
-    if (*s1 == *s2) /* if chars match, increment both strings */
-        return (wildcmp(s1 + 1, s2 + 1));
-
-    if (*s1 != *s2 && *s2 == '*') /* if wildcard, try both advancing and not advancing s1 */
-        return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-
-    return (0); /* if no match and no wildcard, return 0 */
+	return (str_checker(s1, s2, 0, 0));
 }
