25c9edd364b3a416e8acffb1c7f5bfa59bedbf05
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-05 14:43:29 -0700

    Update 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,22 +9,22 @@
  */
 int check_prime(int n, int i)
 {
-    if (n <= 1)
-    {
-        return (0);
-    }
-    else if (n % i == 0)
-    {
-        return (0);
-    }
-    else if (i * i > n)
-    {
-        return (1);
-    }
-    else
-    {
-        return (check_prime(n, i + 1));
-    }
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (i * i > n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (check_prime(n, i + 1));
+	}
 }
 
 /**
@@ -35,5 +35,5 @@ int check_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-    return (check_prime(n, 2));
+	return (check_prime(n, 2));
 }
