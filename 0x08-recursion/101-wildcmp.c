6ab4f67659c7b17bf4aacd46bc2a8d8f707f59f0
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-05 14:48:08 -0700

    Update 101-wildcmp.c file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,35 +1,29 @@
 #include "main.h"
 
 /**
- * wildcard_match - helper function to handle wildcard matches
+ * check_match - helper function to handle wildcard matches
  * @s1: string to compare
  * @s2: string to compare with possible wildcard(s)
- * @wildcard: flag to track if wildcard has been encountered
- * 
+ * @wild: flag to track if wildcard has been encountered
+ *
  * Return: 1 if match, 0 if not
  */
-int wildcard_match(char *s1, char *s2, int wildcard)
+int check_match(char *s1, char *s2, char *wild)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	if (*s2 == '*')
-		return (wildcard_match(s1, s2 + 1, 1));
-
-	if (*s1 == '\0' && *s2 != '\0' && wildcard == 0)
-		return (0);
-
-	if (*s1 == '\0' && *s2 != '\0' && *s2 != '*' && wildcard == 1)
-		return (0);
+	if (*s1 == '\0' && *s2 == '*' && wild == 0)
+		return (check_match(s1, s2 + 1, s2));
 
 	if (*s1 == *s2)
-		return (wildcard_match(s1 + 1, s2 + 1, 0));
+		return (check_match(s1 + 1, s2 + 1, 0));
 
-	if (*s1 != *s2 && wildcard == 1)
-		return (wildcard_match(s1 + 1, s2, 1));
+	if (*s1 != *s2 && *s2 == '*')
+		return (check_match(s1, s2 + 1, s2));
 
-	if (*s1 != *s2 && wildcard == 0)
-		return (0);
+	if (wild)
+		return (check_match(s1 + 1, wild, wild));
 
 	return (0);
 }
@@ -38,10 +32,10 @@ int wildcard_match(char *s1, char *s2, int wildcard)
  * wildcmp - compares two strings considering wildcard
  * @s1: string to compare
  * @s2: string to compare with possible wildcard(s)
- * 
+ *
  * Return: 1 if match, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (wildcard_match(s1, s2, 0));
+	return (check_match(s1, s2, 0));
 }
