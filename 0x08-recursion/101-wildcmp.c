4002c5a0b10fed2a48a2b6072ab06615392c7a9e
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-05 14:50:28 -0700

    Update 101-wildcmp.c file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,29 +7,16 @@
  *
  * Return: 1 if considered identical, otherwise 0
  */
-
 int wildcmp(char *s1, char *s2)
 {
-    /* if the end of both strings is reached */
-    if (*s1 == '\0' && *s2 == '\0')
+    if (*s1 == '\0' && *s2 == '\0') /* if end of both strings, they match */
         return (1);
-    /* comparing each character of both strings */
-    if (*s1 == *s2)
+
+    if (*s1 == *s2) /* if chars match, increment both strings */
         return (wildcmp(s1 + 1, s2 + 1));
-    /* when '*' is found in s2 */
-    if (*s2 == '*')
-    {
-        /* move the s2 pointer until it points to a character other than '*' */
-        s2 = s2 + 1;
-        if (*s2 == '\0')
-            return (1);
-        /* call the function recursively for every match after '*' */
-        while (*s1 != '\0')
-        {
-            if (wildcmp(s1, s2))
-                return (1);
-            s1++;
-        }
-    }
-    return (0);
+
+    if (*s1 != *s2 && *s2 == '*') /* if wildcard, try both advancing and not advancing s1 */
+        return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+
+    return (0); /* if no match and no wildcard, return 0 */
 }
