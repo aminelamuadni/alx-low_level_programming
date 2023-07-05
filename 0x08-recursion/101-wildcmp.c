ccbe1b574192cec536ef2907b592814e97e2d47c
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-05 14:49:36 -0700

    Update 101-wildcmp.c file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,41 +1,35 @@
 #include "main.h"
 
 /**
- * check_match - helper function to handle wildcard matches
- * @s1: string to compare
- * @s2: string to compare with possible wildcard(s)
- * @wild: flag to track if wildcard has been encountered
- *
- * Return: 1 if match, 0 if not
- */
-int check_match(char *s1, char *s2, char *wild)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == '\0' && *s2 == '*' && wild == 0)
-		return (check_match(s1, s2 + 1, s2));
-
-	if (*s1 == *s2)
-		return (check_match(s1 + 1, s2 + 1, 0));
-
-	if (*s1 != *s2 && *s2 == '*')
-		return (check_match(s1, s2 + 1, s2));
-
-	if (wild)
-		return (check_match(s1 + 1, wild, wild));
-
-	return (0);
-}
-
-/**
- * wildcmp - compares two strings considering wildcard
+ * wildcmp - function that compares two strings
  * @s1: string to compare
  * @s2: string to compare with possible wildcard(s)
  *
- * Return: 1 if match, 0 if not
+ * Return: 1 if considered identical, otherwise 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	return (check_match(s1, s2, 0));
+    /* if the end of both strings is reached */
+    if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+    /* comparing each character of both strings */
+    if (*s1 == *s2)
+        return (wildcmp(s1 + 1, s2 + 1));
+    /* when '*' is found in s2 */
+    if (*s2 == '*')
+    {
+        /* move the s2 pointer until it points to a character other than '*' */
+        s2 = s2 + 1;
+        if (*s2 == '\0')
+            return (1);
+        /* call the function recursively for every match after '*' */
+        while (*s1 != '\0')
+        {
+            if (wildcmp(s1, s2))
+                return (1);
+            s1++;
+        }
+    }
+    return (0);
 }
