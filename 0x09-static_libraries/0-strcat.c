cc8466a9ac6eeb010becd54d9bebf20a582bdfa8
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-07 08:46:43 -0700

    Add 0-strcat.c file

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	/* Find the end of the destination string */
+	while (dest[i])
+		i++;
+
+	/* Append the source string to the destination string */
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	/* Null-terminate the destination string */
+	dest[i + j] = '\0';
+
+	return (dest);
+}
