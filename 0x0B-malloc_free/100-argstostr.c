09da6c350ede47551dc6db93033babdd74ba04a2
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-11 18:03:46 -0700

    Add 100-argstostr.c file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+
+	return (str);
+}
