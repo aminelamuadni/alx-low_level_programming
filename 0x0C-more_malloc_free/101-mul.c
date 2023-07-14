f51fda3d97136977a355b47bcb4419520752629b
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 19:47:58 -0700

    Update 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,124 +1,134 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * check_zero - checks if either of the numbers is zero
- * @argv: command line arguments
+ * _memset - fills memory with a constant byte
  *
- * Return: void
- */
-void check_zero(char *argv[])
+ * @s: input pointer that represents memory block
+ *     to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i, n1 = 1, n2 = 1;
+	unsigned int i = 0;
 
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			n1 = 0;
-			break;
-		}
-
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			n2 = 0;
-			break;
-		}
-
-	if (n1 == 1 || n2 == 1)
+	while (i < n)
 	{
-		printf("0\n");
-		exit(0);
+		s[i] = b;
+		i++;
 	}
+	return (s);
 }
 
 /**
- * initialize_array - initializes an array with '0'
- * @ar: the array to initialize
- * @lar: the length of the array
+ * _calloc - function that allocates memory
+ *           for an array using memset
  *
- * Return: the initialized array
- */
-char *initialize_array(char *ar, int lar)
-{
-	int i = 0;
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer to new allocated memory
+*/
 
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
 
-/**
- * check_num - checks if the given argument is a valid number
- * @argv: command line arguments
- * @n: index of the argument to check
- *
- * Return: the length of the number
- */
-int check_num(char *argv[], int n)
-{
-	int length;
 
-	for (length = 0; argv[n][length]; length++)
-		if (!isdigit(argv[n][length]))
+/**
+ * multiply - initialize array with 0 byte
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: nothing
+*/
+
+void multiply(char *s1, char *s2)
+{
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
+
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = _calloc(sizeof(int), total_l);
+
+	/* store our pointer address to free later */
+	temp = ptr;
+
+	for (l1--; l1 >= 0; l1--)
+	{
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+		for (l2--; l2 >= 0; l2--)
 		{
-			printf("Error\n");
-			exit(98);
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
 		}
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
+	}
 
-	return (length);
+	while (*ptr == 0)
+	{
+		ptr++;
+		total_l--;
+	}
+
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+	free(temp);
 }
 
+
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
+ * main - Entry point
  *
- * Return: 0 on success, 1 on failure
- */
+ * Description: a program that multiplies
+ *            two positive numbers
+ *
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 on success 98 on faliure
+*/
+
 int main(int argc, char *argv[])
 {
-	int len1, len2, len_out, sum, addl, i, j, k, count;
-	char *out;
+	char *n1 = argv[1];
+	char *n2 = argv[2];
 
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-	len1 = check_num(argv, 1), len2 = check_num(argv, 2);
-	check_zero(argv), len_out = len1 + len2, out = malloc(len_out + 1);
-	if (out == NULL)
-		printf("Error\n"), exit(98);
-	out = initialize_array(out, len_out);
-	k = len_out - 1, i = len1 - 1, j = len2 - 1, count = carry = 0;
-	for (; k >= 0; k--, i--)
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
+
+	if (*n1 == '0' || *n2 == '0')
 	{
-		if (i < 0)
-		{
-			if (carry > 0)
-			{
-				sum = (out[k] - '0') + carry;
-				if (sum > 9)
-					out[k - 1] = (sum / 10) + '0';
-				out[k] = (sum % 10) + '0';
-			}
-			i = len1 - 1, j--, carry = 0, count++, k = len_out - (1 + count);
-		}
-		if (j < 0)
-		{
-			if (out[0] != '0')
-				break;
-			len_out--;
-			free(out), out = malloc(len_out + 1), out = initialize_array(out, len_out);
-			k = len_out - 1, i = len1 - 1, j = len2 - 1, count = carry = 0;
-		}
-		if (j >= 0)
-		{
-			sum = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (out[k] - '0') + carry;
-			carry = sum / 10, out[k] = (sum % 10) + '0';
-		}
+		_putchar('0');
+		_putchar('\n');
 	}
-	printf("%s\n", out);
+	else
+		multiply(n1, n2);
 	return (0);
 }
