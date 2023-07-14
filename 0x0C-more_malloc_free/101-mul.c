83b2b391f27539ebde9922920d95bcacf109d302
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 18:35:32 -0700

    Update 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,100 +1,64 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * print_error - Prints "Error" followed by a new line to stderr
+ * is_digit - checks if a string contains only digits
+ * @s: string to check
  *
- * Return: void
+ * Return: 0 if string contains non-digits, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i, length = strlen(s);
+
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * print_error - prints error message and exits program
  */
 void print_error(void)
 {
+	int i;
 	char *error = "Error\n";
 
-	while (*error)
-		_putchar(*error++);
+	for (i = 0; error[i]; i++)
+	{
+		_putchar(error[i]);
+	}
 
 	exit(98);
 }
 
 /**
- * multiply - Multiplies two positive numbers represented as strings
- * @num1: The first number
- * @num2: The second number
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: The result of the multiplication as a string
- */
-char *multiply(char *num1, char *num2)
-{
-	int len1, len2, len, i, j, prod, carry;
-	int *result;
-	char *mul;
-
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len = len1 + len2;
-	result = calloc(len, sizeof(int));
-	if (!result)
-		print_error();
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			prod = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1];
-			result[i + j + 1] = prod % 10;
-			carry = prod / 10;
-		}
-		result[i] += carry;
-	}
-
-	if (result[0] == 0)
-	{
-		mul = malloc(len);
-		if (!mul)
-			print_error();
-
-		for (i = 1, j = 0; i <= len; i++, j++)
-			mul[j] = result[i] + '0';
-		mul[j] = '\0';
-	}
-	else
-	{
-		mul = malloc(len + 1);
-		if (!mul)
-			print_error();
-
-		for (i = 0, j = 0; i < len; i++, j++)
-			mul[j] = result[i] + '0';
-		mul[j] = '\0';
-	}
-
-	free(result);
-	return (mul);
-}
-
-/**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
- *
- * Return: 0 on success, 98 on failure
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2, *result;
+	long num1, num2, product;
 
-	if (argc != 3)
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	{
 		print_error();
+	}
 
-	num1 = argv[1];
-	num2 = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
 
-	if (!_isdigit(num1) || !_isdigit(num2))
-		print_error();
-
-	result = multiply(num1, num2);
-	_print(result);
-	free(result);
+	printf("%ld\n", product);
 
 	return (0);
 }
