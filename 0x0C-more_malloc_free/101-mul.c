ff24cf4fbca4925e258c33c80268249409a4ed30
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 18:29:04 -0700

    Update 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,70 +1,99 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * error - prints an error message and exits with status 98
+ * print_error - Prints "Error" followed by a new line to stderr
+ *
+ * Return: void
  */
-void error(void)
+void print_error(void)
 {
-	printf("Error\n");
+	char *error = "Error\n";
+
+	while (*error)
+		_putchar(*error++);
+
 	exit(98);
 }
 
 /**
- * is_digit - checks if a string only contains digits
- * @s: the string to check
+ * multiply - Multiplies two positive numbers represented as strings
+ * @num1: The first number
+ * @num2: The second number
  *
- * Return: 1 if the string only contains digits, otherwise 0
+ * Return: The result of the multiplication as a string
  */
-int is_digit(char *s)
+char *multiply(char *num1, char *num2)
 {
-	int i = 0;
+	int len1, len2, len, i, j, prod, carry;
+	int *result;
+	char *mul;
 
-	while (s[i])
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
+	result = calloc(len, sizeof(int));
+	if (!result)
+		print_error();
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			prod = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1];
+			result[i + j + 1] = prod % 10;
+			carry = prod / 10;
+		}
+		result[i] += carry;
 	}
-	return (1);
+
+	if (result[0] == 0)
+	{
+		mul = malloc(len);
+		if (!mul)
+			print_error();
+
+		for (i = 1, j = 0; i <= len; i++, j++)
+			mul[j] = result[i] + '0';
+		mul[j] = '\0';
+	}
+	else
+	{
+		mul = malloc(len + 1);
+		if (!mul)
+			print_error();
+
+		for (i = 0, j = 0; i < len; i++, j++)
+			mul[j] = result[i] + '0';
+		mul[j] = '\0';
+	}
+
+	free(result);
+	return (mul);
 }
 
 /**
- * mul - multiplies two numbers
- * @num1: the first number
- * @num2: the second number
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
  *
- * Return: the result of the multiplication
- */
-char *mul(char *num1, char *num2)
-{
-	/* implement multiplication of num1 and num2 */
-}
-
-/**
- * main - entry point
- * @argc: number of command-line arguments
- * @argv: array of command-line arguments
- *
- * Return: 0 if the program completed successfully, otherwise a status number
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
 	char *num1, *num2, *result;
 
 	if (argc != 3)
-		error();
+		print_error();
 
 	num1 = argv[1];
 	num2 = argv[2];
 
-	if (!is_digit(num1) || !is_digit(num2))
-		error();
+	if (!_isdigit(num1) || !_isdigit(num2))
+		print_error();
 
-	result = mul(num1, num2);
-
-	printf("%s\n", result);
+	result = multiply(num1, num2);
+	_print(result);
 	free(result);
 
 	return (0);
