b2c9d636725142a6e512d5136d014d7051a34d59
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 18:07:33 -0700

    Update 100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@
  * @ptr: pointer to the memory previously allocated
  * @old_size: size, in bytes, of the allocated space for ptr
  * @new_size: new size, in bytes of the new memory block
- * 
+ *
  * Return: pointer to the new memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
