051c1107c24a85ae059eb9a52857f0a612489e36
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 18:20:47 -0700

    Update 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,46 +1,79 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: new size, in bytes of the new memory block
- *
- * Return: pointer to the new memory block
+ * error - print error message and exit program
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void error(void)
 {
-	void *newptr;
+	printf("Error\n");
+	exit(98);
+}
 
-	if (new_size == old_size)
-		return (ptr);
+/**
+ * is_digit - check if a string is a number
+ * @str: string to check
+ * Return: 0 if str is a number, 1 otherwise
+ */
+int is_digit(char *str)
+{
+	int i = 0;
 
-	if (ptr == NULL)
+	if (!str)
+		return (0);
+	while (*(str + i))
 	{
-		newptr = malloc(new_size);
-		if (newptr == NULL)
-			return (NULL);
-
-		return (newptr);
+		if (*(str + i) < '0' || *(str + i++) > '9')
+			return (0);
 	}
+	return (1);
+}
 
-	if (new_size == 0 && ptr != NULL)
+/**
+ * multiply - multiply two numbers and print the result
+ * @n1: first number
+ * @n2: second number
+ */
+void multiply(char *n1, char *n2)
+{
+	int len1, len2, tot, i, j;
+	int *result;
+
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+	tot = len1 + len2;
+
+	result = calloc(tot, sizeof(int));
+	if (!result)
+		error();
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		free(ptr);
-		return (NULL);
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			result[i + j + 1] += ((n1[i] - '0') * (n2[j] - '0'));
+			result[i + j] += result[i + j + 1] / 10;
+			result[i + j + 1] %= 10;
+		}
 	}
+	for (i = *result; !i && tot > 1; tot--)
+		result++;
+	for (i = 0; i < tot; i++)
+		printf("%d", result[i]);
+	printf("\n");
+}
 
-	newptr = malloc(new_size);
-	if (newptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	memcpy(newptr, ptr, old_size < new_size ? old_size : new_size);
-	free(ptr);
-
-	return (newptr);
+/**
+ * main - multiply two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		error();
+	multiply(argv[1], argv[2]);
+	return (0);
 }
