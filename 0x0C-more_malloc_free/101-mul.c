f5506b8193964149dd4aa62de3dde658891d3b1c
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-13 19:39:18 -0700

    Update 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,61 +1,75 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * is_digit - checks if a string contains only digits
- * @s: string to check
+ * check_zero - checks if either of the numbers is zero
+ * @argv: command line arguments
  *
- * Return: 0 if string contains non-digits, 1 otherwise
+ * Return: void
  */
-int is_digit(char *s)
+void check_zero(char *argv[])
 {
-	int i;
+	int i, n1 = 1, n2 = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			n1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			n2 = 0;
+			break;
+		}
+
+	if (n1 == 1 || n2 == 1)
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
+		printf("0\n");
+		exit(0);
 	}
-
-	return (1);
 }
 
 /**
- * multiply - performs multiplication of two positive numbers
- * @num1: first number
- * @num2: second number
+ * initialize_array - initializes an array with '0'
+ * @ar: the array to initialize
+ * @lar: the length of the array
  *
- * Return: pointer to the result of multiplication
+ * Return: the initialized array
  */
-char *multiply(char *num1, char *num2)
+char *initialize_array(char *ar, int lar)
 {
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int len_res = len1 + len2;
-	int i, j, k, carry;
-	char *result = calloc(len_res + 1, sizeof(char));
+	int i = 0;
 
-	if (result == NULL)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
 
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1, k = len_res - (len1 - i); j >= 0; j--, k--)
+/**
+ * check_num - checks if the given argument is a valid number
+ * @argv: command line arguments
+ * @n: index of the argument to check
+ *
+ * Return: the length of the number
+ */
+int check_num(char *argv[], int n)
+{
+	int length;
+
+	for (length = 0; argv[n][length]; length++)
+		if (!isdigit(argv[n][length]))
 		{
-			int product = (num1[i] - '0') * (num2[j] - '0') + (result[k] - '0') + carry;
-
-			carry = product / 10;
-			result[k] = (product % 10) + '0';
+			printf("Error\n");
+			exit(98);
 		}
-		result[k] += carry;
-	}
 
-	return (result);
+	return (length);
 }
 
 /**
@@ -67,23 +81,44 @@ char *multiply(char *num1, char *num2)
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2, *product;
+	int len1, len2, len_out, sum, addl, i, j, k, count;
+	char *out;
 
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	len1 = check_num(argv, 1), len2 = check_num(argv, 2);
+	check_zero(argv), len_out = len1 + len2, out = malloc(len_out + 1);
+	if (out == NULL)
+		printf("Error\n"), exit(98);
+	out = initialize_array(out, len_out);
+	k = len_out - 1, i = len1 - 1, j = len2 - 1, count = carry = 0;
+	for (; k >= 0; k--, i--)
 	{
-		printf("Error\n");
-		return (98);
+		if (i < 0)
+		{
+			if (carry > 0)
+			{
+				sum = (out[k] - '0') + carry;
+				if (sum > 9)
+					out[k - 1] = (sum / 10) + '0';
+				out[k] = (sum % 10) + '0';
+			}
+			i = len1 - 1, j--, carry = 0, count++, k = len_out - (1 + count);
+		}
+		if (j < 0)
+		{
+			if (out[0] != '0')
+				break;
+			len_out--;
+			free(out), out = malloc(len_out + 1), out = initialize_array(out, len_out);
+			k = len_out - 1, i = len1 - 1, j = len2 - 1, count = carry = 0;
+		}
+		if (j >= 0)
+		{
+			sum = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (out[k] - '0') + carry;
+			carry = sum / 10, out[k] = (sum % 10) + '0';
+		}
 	}
-
-	num1 = argv[1];
-	num2 = argv[2];
-	product = multiply(num1, num2);
-
-	while (*product == '0' && *(product + 1) != '\0')
-		product++;
-
-	printf("%s\n", product);
-
-	free(product);
+	printf("%s\n", out);
 	return (0);
 }
