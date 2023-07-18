3521fdc5e946c2627822cecdc335ba2b6318d420
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-17 19:09:46 -0700

    Update dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - new type
  * @name: dog's name
@@ -17,10 +20,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
 
 #endif /* DOG_H */
