e3282e5c8d167a3a22fd8fc5f8e82d11827d519f
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-17 18:59:48 -0700

    Add dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: data about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
