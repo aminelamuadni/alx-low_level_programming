f76083cc264849d43c7f70832be5ded606b06904
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-19 16:43:34 -0700

    Add 3-main.c file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+
+	if (!f || (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", f(num1, num2));
+	return (0);
+}
