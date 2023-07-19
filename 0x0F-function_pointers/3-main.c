fe27c67737e2ea4182b21f450cb060407d492d0d
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-19 16:45:25 -0700

    Update 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,14 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (!f || (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%'))
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	    argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
 		exit(99);
