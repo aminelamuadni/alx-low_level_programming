c40399887a9f804f20771b5ab610f2c8031a49cb
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-27 14:48:15 -0700

    Update 1-list_len.c file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the start of the list
- * 
+ *
  * Return: number of nodes in the list
  */
 size_t list_len(const list_t *h)
