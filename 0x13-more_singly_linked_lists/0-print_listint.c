deb1741183a0734e8a8b0f2627982a7a5eb1b151
Amine Lamuadni <W7equwE5@gmail.com>
2023-07-31 18:45:24 -0700

    Update 0-print_listint.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -10,10 +9,27 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
+	int n, copy;
+	char c;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
+		n = h->n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		copy = n;
+		while (copy /= 10)
+			n = n / 10;
+		while (n > 0)
+		{
+			c = (n % 10) + '0';
+			_putchar(c);
+			n = n / 10;
+		}
+		_putchar('\n');
 		h = h->next;
 		nodes++;
 	}
