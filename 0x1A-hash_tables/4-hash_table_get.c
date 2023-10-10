c0157924e061c928730c03b263c0841eddc5e6dc
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-10 06:35:43 -0700

    Update 4-hash_table_get.c file

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,7 +6,8 @@
  * @ht: the hash table
  * @key: the key
  *
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element, or NULL if
+ * key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
