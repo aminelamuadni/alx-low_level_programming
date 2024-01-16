7748acfa333dee2e7080b74f23bfd4472d331b0f
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-16 08:05:52 -0800

    Update 0-linear.c file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,18 +10,18 @@
  * Return: First index where value is located, or -1 if not found or array is NULL.
  */
 int linear_search(int *array, size_t size, int value) {
-    size_t i;
+	size_t i;
 
-    if (array == NULL) {
-        return -1;
-    }
+	if (array == NULL) {
+		return -1;
+	}
 
-    for (i = 0; i < size; i++) {
-        printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-        if (array[i] == value) {
-            return (int)i;
-        }
-    }
+	for (i = 0; i < size; i++) {
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+		if (array[i] == value) {
+			return (int)i;
+		}
+	}
 
-    return -1;
+	return -1;
 }
