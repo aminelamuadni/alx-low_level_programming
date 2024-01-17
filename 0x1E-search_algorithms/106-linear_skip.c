23769f6abaf22eb31108df33c48b02b570afb200
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-17 04:13:44 -0800

    Update 106-linear_skip.c file

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -15,16 +15,25 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!list)
 		return (NULL);
 
-	while (node && node->express && node->express->n < value)
+	while (node->express && node->express->n < value)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", node->express->index,
 			node->express->n);
 		node = node->express;
 	}
 
-	express = node->express ? node->express : node;
-	while (express->next)
-		express = express->next;
+	if (node->express)
+	{
+		express = node->express;
+		printf("Value checked at index [%lu] = [%d]\n", express->index,
+			express->n);
+	}
+	else
+	{
+		express = node;
+		while (express->next)
+			express = express->next;
+	}
 	printf("Value found between indexes [%lu] and [%lu]\n", node->index,
 		express->index);
 
